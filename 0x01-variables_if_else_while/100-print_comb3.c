9aad605d90f4492a6142331dab592fd891a43557
2nyc365 <danielabel50@yahoo.com>
2022-10-14 18:18:37 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+	int q;
+
+	for (i = 10; i <= 19; i++)
+	{
+		for (q = 10; q <= 19; q++)
+		{
+			if ((q % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((q % 10) + '0');
+				if (i != 18 || q != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

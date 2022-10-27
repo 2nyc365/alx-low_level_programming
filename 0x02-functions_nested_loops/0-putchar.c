9611f50a3318b4bf8cbd503326e1108d90fe6f4d
2nyc365 <danielabel50@yahoo.com>
2022-10-27 10:24:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a newline
+ * main - Prints _putchar followed by a newline
  *
  * Return: Always 0.
  */
@@ -10,10 +10,11 @@ int main(void)
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
 
-		sz = sizeof(str) / sizeof(int);
+	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
 	}
 	_putchar('\n');
 	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int add (int, int);
-void time_table(void);
+void times_table(void);
 int print_sign(int n);
 int _isalpha(int c);
 int _abs(int);
